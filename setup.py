b05dade3b3470e3f69ea0509b7876ced79821b9b
shrutik bole <boleshrutik51@gmail.com>
2024-01-04 19:31:09 +0530

    requirements

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
 import setuptools
 
-with open(README.md, "r", encoding = "utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken_Disease_Classification_CNN"
-AUTHOR_USER_NAME = "shrutikbole"
+REPO_NAME = "Chicken-Disease-Classification--Project"
+AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "boleshrutik51@gmail.com"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
 
 
 setuptools.setup(
-name = SRC_REPO,
-version = __version__,
-author = AUTHOR_USER_NAME,
-author_email = AUTHOR_EMAIL,
-description = "A small python package for CNN app",
-long_description = long_description,
-long_description_content = "text/markdown",
-url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-project_url = {
-    "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
-},
-package_dir = {"": "src"},
-packages = setuptools.find_packages(where="src")
-
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
-
